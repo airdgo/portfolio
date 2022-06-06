1bac4c1b4e33fb436789d51190b040ce9bbdd0b3
Vlad Dragoi <vladdragoi1@gmail.com>
2022-06-06 07:36:10 +0300

    added smooth scrolling between pages

--- layout/Layout.js
+++ layout/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
 	return (
 		<>
 			<Header />
-			<main className="max-h-screen snap-y snap-mandatory overflow-y-scroll">
+			<main className="max-h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth">
 				{children}
 			</main>
 		</>
