957726906ddba794d49583e53d754801cb16053f
Vlad Dragoi <vladdragoi1@gmail.com>
2022-06-21 15:22:21 +0300

    created components for mobileNav and desktopNav

--- sections/Header.js
+++ sections/Header.js
@@ -1,18 +1,13 @@
 import Link from "next/link";
 import { useState } from "react";
-import { navigation, externalLinks } from "../constants";
 import HamburgerMenu from "../components/HamburgerMenu";
-import { useTransition, animated } from "@react-spring/web";
 import { useScrollPosition } from "../hooks";
+import DesktopNav from "../components/DesktopNav";
+import MobileNav from "../components/MobileNav";
 
 export default function Header() {
 	const [isOpen, setIsOpen] = useState(false);
 	const scrollPosition = useScrollPosition();
-	const transitions = useTransition(isOpen, {
-		from: { x: 0, y: -500, opacity: 0 },
-		enter: { x: 0, y: 0, opacity: 1 },
-		leave: { x: 0, y: -500, opacity: 0 },
-	});
 
 	function toggleMenu() {
 		setIsOpen((prevState) => !prevState);
@@ -45,41 +40,8 @@ export default function Header() {
 					<HamburgerMenu onClick={toggleMenu} isOpen={isOpen} />
 				</div>
 
-				{transitions(
-					(style, item) =>
-						item && (
-							<animated.ul
-								style={style}
-								className="fixed z-[0] flex h-screen w-full flex-col items-center justify-center gap-16 bg-neutral text-3xl lg:hidden"
-							>
-								{navigation.map((nav) => (
-									<li key={nav.title} onClick={hideMenu}>
-										<Link href={nav.link} title={nav.title}>
-											{nav.content}
-										</Link>
-									</li>
-								))}
-
-								<li className="flex w-full max-w-[10rem] items-center justify-between lg:gap-16">
-									{externalLinks.map((nav) => (
-										<Link key={nav.title} href={nav.link} title={nav.title}>
-											{nav.content}
-										</Link>
-									))}
-								</li>
-							</animated.ul>
-						)
-				)}
-
-				<ul className="hidden h-auto items-center justify-center gap-16 text-lg lg:static lg:flex">
-					{[...navigation, ...externalLinks].map((nav) => (
-						<li key={nav.title} onClick={hideMenu}>
-							<Link href={nav.link} title={nav.title}>
-								{nav.content}
-							</Link>
-						</li>
-					))}
-				</ul>
+				<MobileNav isOpen={isOpen} onClick={hideMenu} />
+				<DesktopNav onClick={hideMenu} />
 			</nav>
 		</header>
 	);
