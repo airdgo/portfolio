57edb1a88a85d774f298c03a742ffa1567f8f515
Vlad Dragoi <vladdragoi1@gmail.com>
2022-06-24 16:51:27 +0300

    added mountain scene for about page

--- sections/About.js
+++ sections/About.js
@@ -1,3 +1,4 @@
+import MountainScene from "../assets/images/MountainScene";
 import AboutDivider from "../assets/sectionDividers/AboutDivider";
 
 export default function About() {
@@ -5,9 +6,15 @@ export default function About() {
 		<>
 			<section
 				id="about"
-				className="h-screen w-full bg-primary pt-20 text-primary2"
+				className="flex h-screen w-full justify-center bg-primary pt-20 text-primary2"
 			>
-				<h1 className="text-center text-4xl font-bold">ABOUT</h1>
+				<div className="w-[85%]">
+					<h1 className="text-center text-4xl font-bold">ABOUT</h1>
+
+					<div>
+						<MountainScene />
+					</div>
+				</div>
 			</section>
 
 			<section>
