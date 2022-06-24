bf4549890eb6ead8afe9311bb4d6033a5b1f2357
Vlad Dragoi <vladdragoi1@gmail.com>
2022-06-24 15:39:30 +0300

    homepage styling

--- sections/Home.js
+++ sections/Home.js
@@ -8,21 +8,27 @@ export default function Home() {
 				id="home"
 				className="relative flex min-h-screen w-full justify-center overflow-auto bg-dashboard xl:items-center"
 			>
-				<div className="relative mt-40 flex max-h-[60rem] w-[85%] max-w-[90rem] flex-col items-center justify-around text-primary2 lg:justify-between xl:mt-32 xl:flex-row xl:items-start">
-					<div>
-						<h1 className="text-center text-3xl font-bold sm:text-4xl md:text-5xl md:leading-normal xl:text-left">
-							Hi, I'm Vlad. <br /> A
-							<span className="text-accent1"> Frontend</span> Web Developer
-						</h1>
+				<div className="relative mt-28 flex max-h-[60rem] w-[85%] max-w-[90rem] flex-col items-center justify-around text-primary2 lg:justify-between xl:mt-32 xl:flex-row">
+					<div className="xl:w-[70%]">
+						<div className="text-center text-3xl font-bold sm:text-4xl md:text-6xl md:leading-normal xl:text-left">
+							<h1>Hi, I'm Vlad.</h1>
+							<h2 className="text-2xl sm:text-3xl md:text-5xl md:leading-normal">
+								A <span className="text-accent1"> Frontend </span>
+								<span className="inline-block">Web Developer</span>
+							</h2>
+						</div>
 
 						<h2 className="mt-32 mb-8 text-center text-lg text-primary2 sm:w-full sm:text-center sm:text-2xl lg:text-3xl xl:mt-44 xl:text-left">
 							And also a sports enthusiast
 						</h2>
 					</div>
-					<div className="relative mt-14 w-full max-w-xl">
-						<SkateboardingScene className="relative z-10 h-full w-full" />
+					<div className="relative mt-14 w-[70%] max-w-2xl xl:mt-0 xl:self-end">
+						<SkateboardingScene
+							className="relative z-10 h-full w-full"
+							hLine="xl:hidden"
+						/>
 					</div>
-					<hr className="absolute bottom-3 left-0 hidden w-full bg-primary2 sm:bottom-5 sm:border xl:block" />
+					<hr className="absolute bottom-3 left-0 hidden w-full border-2 bg-primary2 sm:bottom-[1.35rem] xl:block" />
 				</div>
 			</section>
 			<section>
