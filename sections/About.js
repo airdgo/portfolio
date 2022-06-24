e1b5f982142c3982691c116744000634df6df918
Vlad Dragoi <vladdragoi1@gmail.com>
2022-06-24 16:41:39 +0300

    about section heading

--- sections/About.js
+++ sections/About.js
@@ -3,8 +3,11 @@ import AboutDivider from "../assets/sectionDividers/AboutDivider";
 export default function About() {
 	return (
 		<>
-			<section id="about" className="h-screen w-full snap-start bg-primary">
-				<div>ABOUT</div>
+			<section
+				id="about"
+				className="h-screen w-full bg-primary pt-20 text-primary2"
+			>
+				<h1 className="text-center text-4xl font-bold">ABOUT</h1>
 			</section>
 
 			<section>
