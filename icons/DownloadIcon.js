a5915c4d7bf226283798558f0bd40b257004a4b7
Vlad Dragoi <vladdragoi1@gmail.com>
2022-06-25 15:27:10 +0300

    styling for big screens

--- icons/DownloadIcon.js
+++ icons/DownloadIcon.js
@@ -1,4 +1,4 @@
-export default function DownloadIcon() {
+export default function DownloadIcon({ ...props }) {
 	return (
 		<svg
 			width="19"
@@ -6,10 +6,11 @@ export default function DownloadIcon() {
 			viewBox="0 0 19 19"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
+			{...props}
 		>
 			<path
 				d="M1.58325 13.4583L2.07488 15.4256C2.1605 15.7682 2.35815 16.0723 2.63643 16.2896C2.91471 16.5069 3.25763 16.6249 3.61071 16.625H15.3891C15.7422 16.6249 16.0851 16.5069 16.3634 16.2896C16.6417 16.0723 16.8393 15.7682 16.925 15.4256L17.4166 13.4583M9.49992 11.875V2.375V11.875ZM9.49992 11.875L6.33325 8.70833L9.49992 11.875ZM9.49992 11.875L12.6666 8.70833L9.49992 11.875Z"
-				stroke="black"
+				stroke="#11171E"
 				strokeWidth="2"
 				strokeLinecap="round"
 				strokeLinejoin="round"
--- sections/About.js
+++ sections/About.js
@@ -7,34 +7,44 @@ export default function About() {
 		<>
 			<section
 				id="about"
-				className="flex min-h-screen w-full justify-center bg-primary pt-16 text-primary2"
+				className="flex min-h-screen w-full justify-center bg-primary pt-16 text-primary2 xl:items-center"
 			>
-				<div className="flex min-h-full w-[85%] flex-col justify-around">
-					<h1 className="text-center text-4xl font-bold">ABOUT</h1>
-					<div className="my-">
-						<MountainScene />
-					</div>
-					<div>
+				<div className="flex min-h-full w-[85%] max-w-[90rem] flex-col xl:max-h-[30rem] xl:flex-row xl:justify-between">
+					<div className="flex min-h-full flex-col items-center justify-around xl:items-start xl:justify-between">
+						<h1 className="text-center text-4xl font-bold sm:text-4xl md:text-7xl">
+							ABOUT
+						</h1>
+						<div className="my-4 w-full max-w-xl xl:hidden">
+							<MountainScene />
+						</div>
 						<div>
-							<h2 className="font-accent text-3xl font-medium">Explorer</h2>
-							<p className="mt-4 text-sm text-neutralLight">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-								dapibus non elit vitae porttitor. Maecenas sodales massa risus,
-								a sollicitudin est condimentum sit amet.
-							</p>
+							<div className="mb-6 md:mb-12 xl:mb-16">
+								<h2 className="font-accent text-3xl font-medium md:text-4xl lg:text-5xl">
+									Explorer
+								</h2>
+								<p className="mt-4 max-w-xl text-sm text-neutralLight md:mt-6 md:text-base lg:mt-8">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+									Aliquam dapibus non elit vitae porttitor. Maecenas sodales
+									massa risus, a sollicitudin est condimentum sit amet.
+								</p>
+							</div>
+							<div className="flex items-center md:text-lg">
+								Download my
+								<a
+									className="ml-4 flex items-center gap-3 rounded-full bg-accent2 py-1 px-4 font-accent text-primary transition-colors duration-300 hover:bg-primary2"
+									title="Download Resume"
+									href="/Vlad-Dragoi-Resume.pdf"
+									target="_blank"
+								>
+									resume
+									<DownloadIcon />
+								</a>
+							</div>
 						</div>
-						<div className="mt-6 flex items-center">
-							Download my
-							<a
-								className="ml-4 flex items-center gap-3 rounded-full bg-accent2 py-1 px-4 font-accent"
-								title="Download Resume"
-								href="/Vlad-Dragoi-Resume.pdf"
-								target="_blank"
-							>
-								resume
-								<DownloadIcon />
-							</a>
-						</div>
+					</div>
+
+					<div className="hidden w-full max-w-2xl xl:block">
+						<MountainScene />
 					</div>
 				</div>
 			</section>
