b43cd14b11351b43def66c3383687526567c52ec
Vlad Dragoi <vladdragoi1@gmail.com>
2022-06-27 15:50:58 +0300

    added background color for html tag

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html className="scroll-smooth">
+		<Html className="scroll-smooth bg-dashboard">
 			<Head />
 			<body>
 				<Main />
