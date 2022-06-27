2df420e6f6cc3fe0c5f50747698b4bc07f68c4a5
Vlad Dragoi <vladdragoi1@gmail.com>
2022-06-27 14:49:17 +0300

    styling for large screens

--- components/Input.js
+++ components/Input.js
@@ -1,8 +1,24 @@
-export default function Input({ ...props }) {
+import { classNames } from "../utils";
+
+export default function Input({ label, ...props }) {
+	const inputClass =
+		"mb-2 mt-1 block rounded bg-primaryLight px-2 py-1 focus:outline focus:outline-1 focus:outline-neutralLight sm:mb-8 sm:py-2 lg:mb-14 lg:mt-2";
+
 	return (
-		<input
-			{...props}
-			className="mb-2 mt-1 block rounded bg-primaryLight px-2 py-1 focus:outline focus:outline-1 focus:outline-neutralLight"
-		/>
+		<>
+			{label && (
+				<label htmlFor={props?.name} className="md:text-lg">
+					{label}
+				</label>
+			)}
+			{props?.name === "message" ? (
+				<textarea
+					{...props}
+					className={classNames(inputClass, "resize-none sm:h-24")}
+				></textarea>
+			) : (
+				<input {...props} className={inputClass} />
+			)}
+		</>
 	);
 }
--- sections/Contact.js
+++ sections/Contact.js
@@ -8,37 +8,30 @@ export default function Contact() {
 	return (
 		<>
 			<Section id="contact" addClass="" bgColor="bg-primary">
-				<Container addClass="flex-col">
-					<div className="">
+				<Container addClass="flex-col justify-around items-center lg:justify-between lg:flex-row">
+					<div className="lg:mr-8 lg:w-full lg:min-w-[30rem]">
 						<h1 className="mb-16 text-4xl font-bold md:mb-20 md:text-6xl lg:text-7xl xl:mb-24">
 							CONTACT
 						</h1>
-						<div className="mb-6 md:mb-12 xl:mb-16">
-							<h2 className="font-accent text-2xl font-medium md:text-4xl lg:text-5xl">
+						<div>
+							<h2 className="font-accent text-2xl font-medium md:text-4xl">
 								Doler ipsum sit amet
 							</h2>
 							<p className="mt-4 max-w-xl text-sm text-neutralLight md:mt-6 md:text-base lg:mt-8">
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
 								dapibus non elit vitae porttitor.
 							</p>
-							<div className="mt-4 flex items-center">
+							<div className="mt-4 flex items-center lg:mt-8">
 								<EmailIcon className="mr-2" /> vladdragoi1@gmail.com
 							</div>
 						</div>
 					</div>
-					<div className="flex w-full items-center justify-center">
-						<form className="flex w-full max-w-md flex-col justify-center">
-							<label for="name">NAME</label>
-							<Input type="text" id="name" name="name" />
-							<label for="email">EMAIL</label>
-							<Input type="email" id="email" name="email" />
-							<label for="message">MESSAGE</label>
-							<textarea
-								id="message"
-								name="message"
-								className="mb-2 mt-1 block resize-none rounded bg-primaryLight px-2 py-1 focus:outline focus:outline-1 focus:outline-neutralLight"
-							></textarea>
-							<button className="mx-auto mt-4 block border-2 px-6 py-1 font-semibold focus:outline focus:outline-1 focus:outline-neutralLight">
+					<div className="flex w-full items-center justify-center lg:justify-end">
+						<form className="flex w-full max-w-sm flex-col justify-center sm:max-w-md lg:max-w-lg">
+							<Input type="text" id="name" name="name" label="NAME" />
+							<Input type="email" id="email" name="email" label="EMAIL" />
+							<Input id="message" name="message" label="MESSAGE" />
+							<button className="mx-auto mt-4 block border px-6 py-1 font-semibold focus:outline focus:outline-1 focus:outline-neutralLight lg:mr-auto lg:ml-0 lg:px-8 lg:py-2">
 								HIT ME UP
 							</button>
 						</form>
