af035d1fb5f5422f5ff73a2cedc6c6e8b06a5d7c
Vlad Dragoi <vladdragoi1@gmail.com>
2022-07-22 16:05:13 +0300

    final polishies

--- sections/Home.js
+++ sections/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
 				addClass="sticky top-0 lg:items-center"
 				bgColor="bg-dashboard"
 			>
-				<Container addClass="flex-col items-center justify-around mt-12 lg:justify-between lg:flex-row sm:mt-16 lg:mt-0 md:mt-8">
+				<Container addClass="flex-col items-center justify-around lg:justify-between lg:flex-row">
 					<div className="lg:min-w-[30rem]">
 						<div className="text-center text-3xl font-bold sm:text-4xl md:text-6xl md:leading-normal lg:text-left">
 							<h1>Hi, I'm Vlad.</h1>
