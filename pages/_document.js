53f4d417d1bbe4a60b942f88c7b1a4993ea3ab06
airdog <vladdragoi1@gmail.com>
2026-04-23 09:57:05 +0300

    remove smooth scroll

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html className="scroll-smooth bg-dashboard" lang="en">
+		<Html className="bg-dashboard" lang="en">
 			<Head />
 			<body>
 				<Main />
